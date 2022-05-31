06205146d088e64fc4a2d253eccd43263407efbc
MUHAMMAD USAMA ASHRAF <75466537+USAMA-AS@users.noreply.github.com>
2022-05-31 22:53:20 +0500

    Text Converter
    Quickly convert your text or paragraph to Uppercase, lowerCase, Remove extra spaces from your paragraph all free and then you can copy your text to clipboard. This app will aslo tells you that how many words and characters are there in your paragraph and how many time it will consume to read all the text. and updated text will also preview on bottom.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Alert from "./components/Alert";
+import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForms from "./components/TextForms";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 
 function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null)
+    }, 1500);
+  }
+  const toggleMode = ()=>{
+    if(mode=== 'light'){
+      setMode('dark');
+      document.body.style.background = '#080f26';
+      showAlert("Dark mode has been enabled", "Success")
+    }
+    else{
+      setMode('light')
+      document.body.style.background = 'white';
+      showAlert("Light mode has been enabled", "Success")
+
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navbar title="Text Converter" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
+      <div className="container my-3">
+      {/* <Switch>
+          <Route exact path="/about">
+            <About />
+          </Route>
+         
+          <Route exact path="/">
+          <TextForms showAlert={showAlert} heading="Enter the Text: " mode={mode}/>
+          </Route>
+        </Switch> */}
+
+        <Routes>
+        <Route path="/about" element={<About />} />
+        <Route path="/" element={<TextForms showAlert={showAlert} heading="Enter the Text: " mode={mode}/>} />
+      </Routes>
+      
+      </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="px-4 py-5 my-5 text-center">
+    <h1 className="display-5 fw-bold">Text Converter</h1>
+    <div className="col-lg-6 mx-auto">
+      <p className="lead mb-4">Quickly convert your text or paragraph to Uppercase, lowerCase, Remove extra spaces from your paragraph all free and then you can copy your text to clipboard. This app will aslo tells you that how many words and characters are there in your paragraph and how many time it will consume to read all the text. and updated text will also preview on bottom. </p>
+      <div className="d-grid gap-2 d-sm-flex justify-content-sm-center">
+       
+      </div>
+    </div>
+  </div>
+  );
+}
